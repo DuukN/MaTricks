113a943bc2f15b08e8def0a692aac04518db80b6
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2021-04-15 11:37:22 +0200

    Improve error handling

--- src/main/java/com/github/duukpn/matricks/MaTricks.java
+++ src/main/java/com/github/duukpn/matricks/MaTricks.java
@@ -7,21 +7,19 @@ public class MaTricks {
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String... args) {
+        System.out.println("----------------------=*=----------------------");
+        System.out.println("Welcome to MaTricks! Please enter a command.\n");
+        while (true) {
+            Scanner command = new Scanner(input.nextLine());
+            if (!command.hasNext()) continue;
 
-        System.out.println("Welcome to MaTricks! Please enter a command.");
-
-        String[] command = input.nextLine().split("\\s+");
-
-        try {
-            switch (command[0]) {
+            switch (command.next()) {
                 case "q":
                 case "quit":
                     return;
                 default:
-                    System.err.println("Syntax error");
+                    System.err.println("Syntax error: unknown command");
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println("Syntax error");
         }
     }
 
